ae58f68d096c97e7803ad41b0fb8b60fba89afe7
johnrobertmahlan <johnrobertmahlan@gmail.com>
2021-03-08 15:54:50 -0700

    first commit

---
+++ app/javascript/packs/weather.js
@@ -0,0 +1,12 @@
+function domReady(fn) {
+    // If we're early to the party
+    document.addEventListener("DOMContentLoaded", fn);
+    // If late; I mean on time.
+    if (document.readyState === "interactive" || document.readyState === "complete" ) {
+      fn();
+    }
+  }
+  
+  domReady(() => console.log("DOM is ready, come and get it!"));
+
+  alert('JS is loaded');
